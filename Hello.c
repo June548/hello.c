01637bba909deb46351f25393208f62cc1571d38
June548 <123063855+June548@users.noreply.github.com>
2023-01-19 16:14:04 +0300

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,9 @@
+//first program in c
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
---
+++ Steph june.c
@@ -0,0 +1,9 @@
+//first program in c
+#include <stdio.h>
+
+int main()
+{
+    printf("steph june");
+
+    return 0;
+}
